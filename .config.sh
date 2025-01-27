8a5bca819e0119999c68969c32d166beceb2a83a
Segun Famisa <segunfamisa@gmail.com>
2025-01-27 14:54:05 +0100

    Add perfetto helper tool
    Add perfetto helper tool

--- .config.sh
+++ .config.sh
@@ -12,6 +12,16 @@ eval "$(jenv init -)"
 export ANDROID_HOME=~/Library/Android/sdk
 export PATH="$ANDROID_HOME/platform-tools:$PATH"
 
+# Perfetto set up
+PERFETTO_HOME=$HOME/.perfetto
+PERFETTO_RECORD_TRACE=$PERFETTO_HOME/record_android_trace
+if [ ! -f $PERFETTO_RECORD_TRACE ]; then
+  mkdir -p $PERFETTO_HOME
+  curl -o $PERFETTO_HOME/record_android_trace https://raw.githubusercontent.com/google/perfetto/main/tools/record_android_trace
+fi
+chmod +x $HOME/.perfetto/record_android_trace
+export PATH="$PERFETTO_HOME:$PATH"
+
 # pyenv/python set up
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
