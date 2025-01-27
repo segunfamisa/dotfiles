2cc12d9f0b878ce393e0084f759efc4c2a2fa44a
Segun Famisa <segunfamisa@gmail.com>
2025-01-27 15:48:34 +0100

    Update .config.sh

--- .config.sh
+++ .config.sh
@@ -15,11 +15,20 @@ export PATH="$ANDROID_HOME/platform-tools:$PATH"
 # Perfetto set up
 PERFETTO_HOME=$HOME/.perfetto
 PERFETTO_RECORD_TRACE=$PERFETTO_HOME/record_android_trace
+PERFETTO_HEAP_PROFILE=$PERFETTO_HOME/heap_profile
+# download record trace
 if [ ! -f $PERFETTO_RECORD_TRACE ]; then
   mkdir -p $PERFETTO_HOME
-  curl -o $PERFETTO_HOME/record_android_trace https://raw.githubusercontent.com/google/perfetto/main/tools/record_android_trace
+  curl -LO $PERFETTO_RECORD_TRACE https://raw.githubusercontent.com/google/perfetto/main/tools/record_android_trace
 fi
+# download heap profile
+if [ ! -f $PERFETTO_HEAP_PROFILE ]; then
+  mkdir -p $PERFETTO_HOME
+  curl -LO $PERFETTO_HEAP_PROFILE https://raw.githubusercontent.com/google/perfetto/main/tools/heap_profile
+fi
+
 chmod +x $HOME/.perfetto/record_android_trace
+chmod +x $HOME/.perfetto/heap_profile
 export PATH="$PERFETTO_HOME:$PATH"
 
 # pyenv/python set up
