a596b618be26f622a74f84a195f5a08e4b4e3c57
Segun Famisa <segunfamisa@gmail.com>
2026-05-27 01:07:05 +0200

    Update fnm integration

--- dot_config/dotfiles/toolchains/node.sh
+++ dot_config/dotfiles/toolchains/node.sh
@@ -1,7 +1,7 @@
 # Node.js: fnm
 
 if command -v fnm >/dev/null 2>&1; then
-  eval "$(fnm env --shell zsh)"
+  eval "$(fnm env --use-on-cd --shell zsh)"
 elif [ -x /opt/homebrew/opt/fnm/bin/fnm ]; then
-  eval "$(/opt/homebrew/opt/fnm/bin/fnm env --shell zsh)"
+  eval "$(/opt/homebrew/opt/fnm/bin/fnm env --use-on-cd --shell zsh)"
 fi
